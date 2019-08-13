e017cb4c5369b9d35e95531be3e26fc32ba92fe9
Floreth Gonzalez <florethgonzalez@Floreths-MacBook-Pro.local>
2019-08-12 23:38:07 -0500

    Repositorio de Lenguajes de Programacion

---
+++ lab3/problem_3c2.c
@@ -0,0 +1,9 @@
+#define Voltage 5
+
+float resistor_fun(float i){
+float b;
+b=Voltage/i;
+return b;
+}
+
+
---
+++ lab3/prog01.c
@@ -0,0 +1,14 @@
+/* My first code in C
+Floreth Gonzalez
+*/
+
+/* Instructions starting with pound (#) are preprocessor commands */
+
+#define BIGNUM 1000000
+#include <stdio.h>
+
+int main (void){
+int a = BIGNUM;
+printf("Hello, world I am %d happy units\n", a);
+return 0;
+}
